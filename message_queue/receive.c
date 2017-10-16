d0c7e807cd33016f4b88541fbc4a98bb438935dc
lallaw8809 <lalbosco55@gmail.com>
2017-10-16 10:46:36 +0530

    Message Queue Sample code

---
+++ message_queue/receive.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<string.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include<string.h>
+#include <stdlib.h>
+
+#define BUF_LEN 128
+
+struct msgbuf {
+	long mtype;       /* message type, must be > 0 */
+	char mtext[BUF_LEN];    /* message data */
+};
+
+/* print the error message */
+void error_meassage(char *p)
+{
+	printf("%s\n",p);
+	exit(0);
+}
+
+void main()
+{
+	key_t key = 1324;
+	int msgid;
+	struct msgbuf data;
+	int index;
+
+	/* Create a message queue*/
+	if( msgid = msgget(key, 0666) < 0 ){
+		error_meassage("Failed to open a message queue\n");
+	}
+
+	/* Append the message one by one into a queue */
+	for(index=1;index<5;index++)
+	{
+		/* Receive the messgae */
+		if( msgrcv(msgid, &data, BUF_LEN, (long) index, 0) < 0){
+			error_meassage("Failed to receive a message\n");	
+		}
+
+		/* Print the received message on the terminal */
+		printf("Received message : %s\n",data.mtext);
+		
+	}
+
+	/* Delete the message queue */
+	//msgctl(msgid,IPC_RMID, NULL);
+}
+
---
+++ message_queue/send.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<string.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <stdlib.h>
+
+#define BUF_LEN 128
+
+struct msgbuf {
+	long mtype;       /* message type, must be > 0 */
+	char mtext[BUF_LEN];    /* message data */
+};
+
+/* print the error message */
+void error_meassage(char *p)
+{
+	printf("%s\n",p);
+	exit(0);
+}
+
+void main()
+{
+	key_t key = 1324;
+	int msgid;
+	struct msgbuf data;
+	int index;
+
+	/* Create a message queue*/
+	if( msgid = msgget(key, IPC_CREAT | 0666) < 0 ){
+		error_meassage("Failed to create a message queue\n");
+	}
+
+	/* Append the message one by one into a queue */
+	for(index=1;index<5;index++)
+	{
+		data.mtype = index;
+		printf("Enter the data\n");
+		scanf("%s",data.mtext);
+
+		/* Send the messgae */
+		if( msgsnd(msgid, &data, strlen(data.mtext)+1, IPC_NOWAIT) < 0){
+			error_meassage("Failed to send a message\n");	
+		}
+	}
+}
+
