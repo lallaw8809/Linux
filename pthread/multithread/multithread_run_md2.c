bad4043753094749661cb24723c3753dee54c57b
lallaw8809 <lalbosco55@gmail.com>
2017-10-19 21:11:53 +0530

    Updated the multithread

--- pthread/multithread/multithread_run_md2.c
+++ pthread/multithread/multithread_run_md2.c
@@ -16,6 +16,8 @@ void *thread2() ;
 void *thread3() ;
 void *thread4() ;
 void *thread5() ;
+void error_message(int error);
+void thread_runing(int thread_num);
 
 void main() 
 {
@@ -23,38 +25,24 @@ void main()
 	int ret,i=0;
 
 	/* Thread 1 creation */
-	ret = pthread_create(&thread_id[0],NULL,&thread1, NULL);
-	if(ret != 0)
-	{
-		printf("Unable to create thread1");
-	}
-
+	if( pthread_create(&thread_id[0],NULL,&thread1, NULL) < 0 )
+		error_message(1);
 
 	/* Thread 2 creation */
-	ret = pthread_create(&thread_id[1],NULL,&thread2,NULL);
-	if(ret != 0)
-	{
-		printf("Unable to create thread2");
-	}
+	if( pthread_create(&thread_id[1],NULL,&thread2,NULL) < 0 )
+		error_message(2);
 
 	/* Thread 3 creation */
-	ret = pthread_create(&thread_id[2],NULL,&thread3, NULL);
-	if(ret != 0)
-	{
-		printf("Unable to create thread3");
-	}
+	if( pthread_create(&thread_id[2],NULL,&thread3, NULL) < 0 )
+		error_message(3);
+
 	/* Thread 4 creation */
-	ret = pthread_create(&thread_id[3],NULL,&thread4, NULL);
-	if(ret != 0)
-	{
-		printf("Unable to create thread4");
-	}
+	if( pthread_create(&thread_id[3],NULL,&thread4, NULL) < 0 )
+		error_message(4);
+
 	/* Thread 5 creation */
-	ret = pthread_create(&thread_id[4],NULL,&thread5, NULL);
-	if(ret != 0)
-	{
-		printf("Unable to create thread5");
-	}
+	if( pthread_create(&thread_id[4],NULL,&thread5, NULL) < 0 )
+		error_message(5);
 
 	/* wait until the created thread to terminate */
 	for(i=0;i<NUM_OF_THREAD;i++)
@@ -64,56 +52,48 @@ void main()
 /* Thread 1 declaration */
 void *thread1() 
 {
-	int index = 0;
-	for(index=0;index<THREAD_RUN;index++)
-	{
-		printf("Thread 1 is running...\n");
-		sleep(1);
-	}
+	thread_runing(1);
 }
 
 /* Thread 2 declaration */
 void *thread2() 
 {
-	int index = 0;
-	for(index=0;index<THREAD_RUN;index++)
-	{
-		printf("Thread 2 is running...\n");
-		sleep(1);
-	}
+	thread_runing(2);
 }
 
 /* Thread 3 declaration */
 void *thread3() 
 {
-	int index = 0;
-	for(index=0;index<THREAD_RUN;index++)
-	{
-		printf("Thread 3 is running...\n");
-		sleep(1);
-	}
+	thread_runing(3);
 }
 
 /* Thread 4 declaration */
 void *thread4() 
 {
-	int index = 0;
-	for(index=0;index<THREAD_RUN;index++)
-	{
-		printf("Thread 4 is running...\n");
-		sleep(1);
-	}
+	thread_runing(4);
 }
 
 /* Thread 5 declaration */
 void *thread5() 
 {
+	thread_runing(5);
+}
+
+/* Execute the thread */
+void thread_runing(int thread_num)
+{
 	int index = 0;
 	for(index=0;index<THREAD_RUN;index++)
 	{
-		printf("Thread 5 is running...\n");
+		printf("Thread %d is running...\n",thread_num);
 		sleep(1);
 	}
 }
 
+/* Print the error message */
+void error_message(int error)
+{
+	printf("Unable to create thread : %d\n",error);
+	exit(-1);
+}
 
