2c6198a3bc48bc87dd9ba67475169326fdcb933f
lallaw8809 <lalbosco55@gmail.com>
2017-10-19 15:17:08 +0530

    Update the multithread

--- pthread/multithread/multithread_run_md1.c
+++ pthread/multithread/multithread_run_md1.c
@@ -1,15 +1,16 @@
 /* Program to create multiple thread and pass the argument
- * All the thread is running in a parral
+ * All the thread is running in a parraly
  * Author : Lal Bosco Lawrence   
  * Date   : 19-oct-2017
  */
+
 
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
 #include<pthread.h>
 
-#define NUM_OF_THREAD 15
+#define NUM_OF_THREAD 3
 #define THREAD_RUN    6
 
 /* Thread fuction declaration */
@@ -18,32 +19,31 @@ void* thread_function(void *arg)
 	int index;
 
 	/* Thread enter into a loop to run concurently */
-	for(index=0;index<THREAD_RUN;index++)
-	{
-		printf("Thread : %d is running\n",*((unsigned int *)arg) );
+	for(index=1;index<THREAD_RUN;index++)
+	{	
+		printf("Thread : %d is running \n",*((unsigned int *)arg));
 		sleep(1);
 	}
+
 }
 
 int main()
 {
 	pthread_t tid[NUM_OF_THREAD];
-	int index = 0,ret;
-	unsigned int thread_num[NUM_OF_THREAD] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+	int index = 0;
+	unsigned int thread_num[NUM_OF_THREAD] ;
+
+	/* Feed the thread count */
+	for(index=0;index<NUM_OF_THREAD;index++)
+		thread_num[index] = index+1;
 
 	/* Create a thread */
 	for(index=0;index<NUM_OF_THREAD;index++)
 	{
-		ret = pthread_create(&tid[index],NULL,thread_function,&thread_num[index]);
-		/* Validation of thread creation */
-		if(ret)
+		if( pthread_create(&tid[index],NULL,thread_function,&thread_num[index]) < 0 )
 		{
 			printf("\nThread %d is not created\n",(index+1) );
 			exit(-1);	
-		}
-		else
-		{
-			printf("\nSucessfully created a thread %d \n",(index+1));
 		}
 	}
 
